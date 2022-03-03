3fa4013a69cf703e5ea4fbd5c9d230599ca68514
billyJoePiano <wicheesemaker@gmail.com>
2022-03-03 14:04:10 -0600

    Added get/set Id methods to LookupPojo interface requirements

--- src/main/nessusData/entity/LookupPojo.java
+++ src/main/nessusData/entity/LookupPojo.java
@@ -1,6 +1,9 @@
 package main.nessusData.entity;
 
 public interface LookupPojo {
+    public int getId();
+    public void setId(int id);
+
     public String toString();
     public void setString(String string);
 }
